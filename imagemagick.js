d5b07a49008e024edb09a7f8b2f7ccc67c85dc02
Thibault BOURGEOIS <thibault@bensound.com>
2021-12-06 14:34:35 +0100

    add fix for parseIdentify https://gist.github.com/cyberwombat/4066782

--- imagemagick.js
+++ imagemagick.js
@@ -103,32 +103,34 @@ function parseIdentify(input) {
       prop = {},
       props = [prop],
       prevIndent = 0,
+      indent = 0,
       indents = [indent],
-      currentLine, comps, indent, i;
+      currentLine, comps;
 
   lines.shift(); //drop first line (Image: name.jpg)
 
   for (i in lines) {
     currentLine = lines[i];
-    indent = currentLine.search(/\S/);
-    if (indent >= 0) {
+    if (currentLine.length > 0) {
       comps = currentLine.split(': ');
-      if (indent > prevIndent) indents.push(indent);
-      while (indent < prevIndent && props.length) {
+      indent = currentLine.search(/\S/);
+      while (indent <= prevIndent) {
         indents.pop();
         prop = props.pop();
         prevIndent = indents[indents.length - 1];
       }
-      if (comps.length < 2) {
+      if(comps.length < 2) {
+        indents.push(indent);
         props.push(prop);
-        prop = prop[currentLine.split(':')[0].trim().toLowerCase()] = {};
-      } else {
-        prop[comps[0].trim().toLowerCase()] = comps[1].trim()
+        prop = prop[comps[0].trim().toLowerCase()] = {};
+        prevIndent = indent;
       }
-      prevIndent = indent;
+      else {
+        prop[comps[0].trim().toLowerCase()] = comps[1].trim();
+      }
     }
   }
-  return prop;
+  return props[0];
 };
 
 exports.identify = function(pathOrArgs, callback) {
